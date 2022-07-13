4e2994ecd029f02a73d08b9aa8efdafb93673c04
Gabriel Teixeira <Gteixeiraca@gmail.com>
2022-07-13 19:43:52 -0300

    Read from file

--- simulador.py
+++ simulador.py
@@ -3,4 +3,8 @@ import sys
 s1, s2, s3 = sys.argv[1], sys.argv[2], sys.argv[3]
 a, b, c = float(s1), float(s2), float(s3)
 
-print(a + b + c)
+file = open('teste.txt', 'r')
+
+fileContent = file.read()
+
+print(fileContent)
